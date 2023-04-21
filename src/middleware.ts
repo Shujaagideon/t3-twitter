25a4017a5db0f0b01e4c8326f35855df241946af
Shujaa Gideon <shujaagideon@gmail.com>
2023-04-21 14:31:51 +0300

    added mongodb and post in prisma

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
  
 export default withClerkMiddleware(() => {
-    console.log('yeey')
   return NextResponse.next();
 });
  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.post.getAll.useQuery();
 
   const user = useUser();
 
@@ -20,9 +20,13 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-[#ededed]">
         <div>
           {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
-          
+          <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
         </div>
-        <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+        <div>
+          {data?.map(post=>(
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
