e33c607942d5b77011bd46c8f6811773b69e2a41
Shujaa Gideon <shujaagideon@gmail.com>
2023-04-26 10:28:45 +0300

    added darkmode 🌑🌚

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  darkMode: 'media',
   theme: {
     fontFamily:{
       'body': ['"Roboto"', 'sans-serif']
